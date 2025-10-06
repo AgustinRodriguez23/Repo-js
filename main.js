e77313a825ca591235808796a537694cf6597228
Agustin Rodriguez <agustinlihuel@gmail.com>
2025-10-05 21:17:23 -0300

    modif alerts

--- main.js
+++ main.js
@@ -1,44 +1,37 @@
 
-let torneo = ligaDelSur
-let edicion = cuarta
-    alert("Bienvenido a "+ torneo + "actualmente disputando la " + edicion +"edicion")
-let jugador = prompt("Bienvenido a Liga del sur, ingresa tu nombre:")
+let torneo = prompt("Ingresa nombre del torneo")
+
+    alert("Bienvenido al torneo "+ torneo + " actualmente disputando la cuarta edicion")
+let jugador = prompt("Ingresa tu nombre:")
 let equipo = prompt("Equipo al que queres seguir:")
     alert("Hola, " + jugador + "!" + " estás siguiendo al equipo " + equipo)
 
+const equipos = ['Parma Sur', 'Los Euca', 'El rejunte', 'Torino', 'Magios', 'Alvarado Fc', 'Madagascar', 'Viejo estado']
+console.log(equipos)
 
-const posiciones = [      
-      { equipo: "Parma Sur", puntos: 30, ganados: 10, empates: 0, posición: 1 },
-      { equipo: "Los Euca", puntos: 25, ganados: 8, empates: 1, posición: 2 },
-      { equipo: "El rejunte", puntos: 20, ganados: 6, empates: 2, posición: 4}
-    ];
+equipos.push ('Fecha libre')
 
-    posiciones.push ({ equipo: "Fecha libre", puntos: 23, ganados: 7, empates: 2, posición: 3})
-
-    for (const equipo of posiciones) {
-    console.log(equipo)
-    }
+for (const equipo of equipos) {
+console.log(equipo)
+}
 
 
+  let suspendido = prompt("Ingrese numero de tarjetas")
+  if (suspendido == 5) {
+   console.log("El jugador está suspendido.")
+  } else {
+    console.log("El jugador puede jugar la fecha con normalidad.")
+  }
 
 
 function sumarPuntos(ganados, empates) {
       return ganados *3 + empates;
     }
 
-    console.log(sumarPuntos(10, 0))
+let puntos = (sumarPuntos(10, 0))
+console.log(puntos)
 
 
-function amonestaciones(numero) {
-  let suspendido = (numero === 5)
-  if (suspendido) {
-    console.log("El jugador está suspendido.")
-  } else {
-    console.log("El jugador puede jugar la fecha con normalidad.")
-  }
-
-}
-amonestaciones(5);    
 
 
 function premiosTorneo(posición){
