55112d63d4c74b2005a9a7817242c0eab45b0a3f
Agustin Rodriguez <agustinlihuel@gmail.com>
2025-10-07 23:52:31 -0300

    Correccion funciones

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ let torneo = prompt("Ingresa nombre del torneo")
 
     alert("Bienvenido al torneo "+ torneo + " actualmente disputando la cuarta edicion")
 let jugador = prompt("Ingresa tu nombre:")
-let equipo = prompt("Equipo al que queres seguir:")
-    alert("Hola, " + jugador + "!" + " estás siguiendo al equipo " + equipo)
+let equipo = prompt("Equipo que representas:")
+   alert("Hola, " + jugador + "!" + " estás siguiendo al equipo " + equipo)
 
 const equipos = ['Parma Sur', 'Los Euca', 'El rejunte', 'Torino', 'Magios', 'Alvarado Fc', 'Madagascar', 'Viejo estado']
 console.log(equipos)
 
 equipos.push ('Fecha libre')
+let inscribirEquipo = prompt("Inscribir equipo para proxima edicion. Nombre de equipo:")
+equipos.push (inscribirEquipo)
+
 
 for (const equipo of equipos) {
 console.log(equipo)
@@ -24,20 +27,24 @@ console.log(equipo)
   }
 
 
+let partidosGanados = parseInt(prompt("Ingrese numero de partidos ganados"))
+let partidosEmpate = parseInt(prompt("Ingrese numero de partidos empatados"))
+
 function sumarPuntos(ganados, empates) {
-      return ganados *3 + empates;
+      let puntosTotales = ganados *3 + empates
+      alert("Tu equipo tiene " + puntosTotales + " puntos")
     }
 
-let puntos = (sumarPuntos(10, 0))
-console.log(puntos)
+ sumarPuntos (partidosGanados, partidosEmpate)
 
 
 
 
+let posiciónFinal = parseInt(prompt("Ingrese posicion final para ver premios. (1,2,3,4)"))
 function premiosTorneo(posición){
     let campeon = (posición === 1)
     if (campeon) {
-        console.log("juego de 15 camisetas, cena para el equipo e reinscripcion gratuita");
+        console.log("juego de 15 camisetas, cena para el equipo y reinscripcion gratuita");
     } else if (posición === 2) {
       console.log("juego de 12 shorts personalizados y pack de 12 cervezas");
     }  else {
@@ -45,4 +52,5 @@ function premiosTorneo(posición){
     }   
 }
 
-premiosTorneo(2)
+premiosTorneo(posiciónFinal)
+
