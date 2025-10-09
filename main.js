aacaa060c50837599a0738b034a60f34b7c5cc42
Agustin Rodriguez <agustinlihuel@gmail.com>
2025-10-09 00:51:46 -0300

    Estilando html

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
 
-let torneo = prompt("Ingresa nombre del torneo")
+//let torneo = prompt("Ingresa nombre del torneo")
 
-    alert("Bienvenido al torneo "+ torneo + " actualmente disputando la cuarta edicion")
-let jugador = prompt("Ingresa tu nombre:")
-let equipo = prompt("Equipo que representas:")
-   alert("Hola, " + jugador + "!" + " estás siguiendo al equipo " + equipo)
+//    alert("Bienvenido al torneo "+ torneo + " actualmente disputando la cuarta edicion")
+//let jugador = prompt("Ingresa tu nombre:")
+//let equipo = prompt("Equipo que representas:")
+//   alert("Hola, " + jugador + "!" + " estás siguiendo al equipo " + equipo)
 
 const equipos = ['Parma Sur', 'Los Euca', 'El rejunte', 'Torino', 'Magios', 'Alvarado Fc', 'Madagascar', 'Viejo estado']
-console.log(equipos)
+//console.log(equipos)
 
-equipos.push ('Fecha libre')
-let inscribirEquipo = prompt("Inscribir equipo para proxima edicion. Nombre de equipo:")
-equipos.push (inscribirEquipo)
-
-
-for (const equipo of equipos) {
-console.log(equipo)
-}
+//equipos.push ('Fecha libre')
+//let inscribirEquipo = prompt("Inscribir equipo para proxima edicion. Nombre de equipo:")
+//equipos.push (inscribirEquipo)
 
 
-  let suspendido = prompt("Ingrese numero de tarjetas")
-  if (suspendido == 5) {
-   console.log("El jugador está suspendido.")
-  } else {
-    console.log("El jugador puede jugar la fecha con normalidad.")
-  }
+//for (const equipo of equipos) {
+//console.log(equipo)
+//}
 
 
-let partidosGanados = parseInt(prompt("Ingrese numero de partidos ganados"))
-let partidosEmpate = parseInt(prompt("Ingrese numero de partidos empatados"))
-
-function sumarPuntos(ganados, empates) {
-      let puntosTotales = ganados *3 + empates
-      alert("Tu equipo tiene " + puntosTotales + " puntos")
-    }
-
- sumarPuntos (partidosGanados, partidosEmpate)
+//  let suspendido = prompt("Ingrese numero de tarjetas")
+//  if (suspendido == 5) {
+//   console.log("El jugador está suspendido.")
+ // } else {
+ //   console.log("El jugador puede jugar la fecha con normalidad.")
+ // }
 
 
+//let partidosGanados = parseInt(prompt("Ingrese numero de partidos ganados"))
+//let partidosEmpate = parseInt(prompt("Ingrese numero de partidos empatados"))
+
+//function sumarPuntos(ganados, empates) {
+//      let puntosTotales = ganados *3 + empates
+//      alert("Tu equipo tiene " + puntosTotales + " puntos")
+ //   }
+//
+// sumarPuntos (partidosGanados, partidosEmpate)
 
 
-let posiciónFinal = parseInt(prompt("Ingrese posicion final para ver premios. (1,2,3,4)"))
-function premiosTorneo(posición){
-    let campeon = (posición === 1)
-    if (campeon) {
-        console.log("juego de 15 camisetas, cena para el equipo y reinscripcion gratuita");
-    } else if (posición === 2) {
-      console.log("juego de 12 shorts personalizados y pack de 12 cervezas");
-    }  else {
-        console.log("medalla de participacion");
-    }   
-}
 
-premiosTorneo(posiciónFinal)
+
+//let posiciónFinal = parseInt(prompt("Ingrese posicion final para ver premios. (1,2,3,4)"))
+//function premiosTorneo(posición){
+//    let campeon = (posición === 1)
+ //   if (campeon) {
+ //       console.log("juego de 15 camisetas, cena para el equipo y reinscripcion gratuita");
+ //   } else if (posición === 2) {
+ //     console.log("juego de 12 shorts personalizados y pack de 12 cervezas");
+  //  }  else {
+ //       console.log("medalla de participacion");
+  //  }   
+//}
+
+//premiosTorneo(posiciónFinal)
 
